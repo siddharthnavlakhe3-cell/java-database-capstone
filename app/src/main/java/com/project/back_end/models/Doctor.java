26482ea26de2b0ecdd4767533eb2aa8a0ac36f45
Siddharth Navlakhe <siddharthnavlakhe3@gmail.com>
2026-05-19 20:24:14 +0530

    Update Doctor.java

--- app/src/main/java/com/project/back_end/models/Doctor.java
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -8,20 +8,17 @@ public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private String name;
     private String specialty;
     private String email;
 
     public Doctor() {}
-    
     public Doctor(Long id, String name, String specialty, String email) {
         this.id = id;
         this.name = name;
         this.specialty = specialty;
         this.email = email;
     }
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
