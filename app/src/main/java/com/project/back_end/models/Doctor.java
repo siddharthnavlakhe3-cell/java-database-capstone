b6b17bad4944ecf56811f7c5d5f0088201256c51
Siddharth Navlakhe <siddharthnavlakhe3@gmail.com>
2026-05-19 19:48:13 +0530

    Create Doctor.java

---
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -0,0 +1,29 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String specialty;
+    private String email;
+
+    public Doctor() {}
+    public Doctor(String name, String specialty, String email) {
+        this.name = name;
+        this.specialty = specialty;
+        this.email = email;
+    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getSpecialty() { return specialty; }
+    public void setSpecialty(String specialty) { this.specialty = specialty; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
